08d254b3413fe2189483be63da54996682d34cc1
hauveli <136408450+hauveli@users.noreply.github.com>
2026-03-28 10:02:32 +0100

    TEST

--- common/src/main/java/hauveli/hexagony/mixin/mindanchor/AddFreshEntityServerLevelMixin.java
+++ common/src/main/java/hauveli/hexagony/mixin/mindanchor/AddFreshEntityServerLevelMixin.java
@@ -1,10 +1,13 @@
 package hauveli.hexagony.mixin.mindanchor;
 
+import dev.architectury.event.events.common.EntityEvent;
 import hauveli.hexagony.mind_anchor.MindAnchorManager;
 import hauveli.hexagony.registry.HexagonyItems;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +17,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 import static hauveli.hexagony.common.blocks.anchors.MindAnchor.TAG_STORED_PLAYER;
+import static org.apache.commons.lang3.math.NumberUtils.toByte;
 
 @Mixin(ServerLevel.class)
 public abstract class AddFreshEntityServerLevelMixin {
@@ -26,6 +30,7 @@ public abstract class AddFreshEntityServerLevelMixin {
         // God forgive me for mixin in to a ServerLevel method like this, I can only pray the check makes it fast
         // Thankfully, I think most of these should fail on the item type comparison...
         if (entity instanceof ItemEntity itemEntity && itemEntity.getItem().getItem() == HexagonyItems.MIND_ANCHOR_FULL.getValue()) {
+            Objects.requireNonNull(entity.getServer()).sendSystemMessage(Component.nullToEmpty("Hello!!!!"));
             if (itemEntity.getServer() != null) {
                 //UUID mindUUID = itemEntity.getItem().getTag().getCompound("BlockEntityTag").getUUID(TAG_STORED_PLAYER);
                 itemEntity.setNoGravity(true);
