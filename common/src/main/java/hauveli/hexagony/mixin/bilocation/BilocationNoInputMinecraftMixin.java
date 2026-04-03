dd02c0859859495af575569f8697a9da5432b97d
hauveli <136408450+hauveli@users.noreply.github.com>
2026-04-03 09:48:50 +0200

    Fix inputs when bilocating

--- common/src/main/java/hauveli/hexagony/mixin/bilocation/BilocationNoInputMinecraftMixin.java
+++ common/src/main/java/hauveli/hexagony/mixin/bilocation/BilocationNoInputMinecraftMixin.java
@@ -1,11 +1,14 @@
 package hauveli.hexagony.mixin.bilocation;
 
 import hauveli.hexagony.common.bilocation.FreeCameraEntity;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
 
 @Mixin(Minecraft.class)
 public abstract class BilocationNoInputMinecraftMixin {
@@ -14,8 +17,24 @@ public abstract class BilocationNoInputMinecraftMixin {
     private void hexagony$disableInputs(CallbackInfo ci) {
         if (FreeCameraEntity.Companion.getActive()) {
             Minecraft mc = Minecraft.getInstance();
-            if (!mc.options.keyChat.isDown())
-                ci.cancel();
+            // if (mc.options.keyChat.isDown()) return;
+            if (mc.options.keyDrop.isDown()) {
+                mc.options.keyDrop.consumeClick();
+            }
+            if (mc.options.keyAttack.isDown()) {
+                mc.options.keyAttack.consumeClick();
+            }
+            if (mc.options.keyUse.isDown()) {
+                mc.options.keyUse.consumeClick();
+            }
+            Arrays.stream(mc.options.keyHotbarSlots).forEach(
+                    keyMapping -> {
+                        if (keyMapping.isDown()) {
+                            keyMapping.consumeClick();
+                        }
+                    }
+            );
+            ci.cancel();
         }
     }
 }
