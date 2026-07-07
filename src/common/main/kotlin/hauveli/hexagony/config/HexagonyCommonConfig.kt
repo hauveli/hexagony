a3cfd44c281c2e44d206cd1f2d7a020e15368b25
hauveli <136408450+hauveli@users.noreply.github.com>
2026-07-07 20:37:41 +0200

    config fixed to be cleaner

--- src/common/main/kotlin/hauveli/hexagony/config/HexagonyCommonConfig.kt
+++ src/common/main/kotlin/hauveli/hexagony/config/HexagonyCommonConfig.kt
@@ -6,6 +6,7 @@ import me.fzzyhmstrs.fzzy_config.annotations.Translation
 import me.fzzyhmstrs.fzzy_config.annotations.Version
 import me.fzzyhmstrs.fzzy_config.config.Config
 import me.fzzyhmstrs.fzzy_config.config.ConfigGroup
+import me.fzzyhmstrs.fzzy_config.config.ConfigSection
 import me.fzzyhmstrs.fzzy_config.util.Walkable
 import me.fzzyhmstrs.fzzy_config.validation.ValidatedField
 import me.fzzyhmstrs.fzzy_config.validation.ValidatedField.Companion.descriptionProvider
@@ -25,26 +26,24 @@ class HexagonyCommonConfig : Config(Hexagony.id("common_config")) {
 
     class HealthcastingOption(mode: OvercastUtils.ValueMode, perInstance: Double, additional: Double): Walkable {
         constructor(): this(OvercastUtils.ValueMode.NONE, 1.0, 1.0)
-
-        var mode = ValidatedEnum(mode, ValidatedEnum.WidgetType.CYCLING)
+        var mode = ValidatedEnum(mode, ValidatedEnum.WidgetType.SCROLLABLE)
         @ValidatedDouble.Restrict(min = 0.0, type = ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
         var perInstance = perInstance // ValidatedDouble(perInstance)
         @ValidatedDouble.Restrict(min = 0.0, type = ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
         var additional = additional // ValidatedDouble(additional)
     }
 
-    var configGroupHealthcasting = ConfigGroup("healthcasting", collapsedByDefault = true)
+    var healthcastingGroup = ConfigGroup("healthcasting_group", collapsedByDefault = true)
     // Healthcasting stuff
     var healthcastingRest = HealthcastingOption(OvercastUtils.ValueMode.CONSTANT, 1.0,0.0)
     var healthcastingPenalty = HealthcastingOption(OvercastUtils.ValueMode.VARIABLE, 1.0,0.0)
+    @ConfigGroup.Pop // a bit confusing that it has to be here and not one below...
     var healthcastingDamage = HealthcastingOption(OvercastUtils.ValueMode.NONE, 0.0,0.0)
-    @ConfigGroup.Pop
-    @ConfigGroup.Pop
 
-
-    var configGroupEnlightenment = ConfigGroup("enlightenment", collapsedByDefault = true)
+    var enlightenmentGroup = ConfigGroup("enlightenment_group", collapsedByDefault = true)
     // Enlightenmnt stuff
     var requireScrollForEnlightenment: ValidatedBoolean = ValidatedBoolean(true)
+    @ConfigGroup.Pop
     var requireScrollForAllGatedSpells: ValidatedCondition<Boolean> = ValidatedBoolean(true)
         .toCondition(
             requireScrollForEnlightenment,
