455b417a7480a745a21010b73e21eedc7c0829f7
hauveli <136408450+hauveli@users.noreply.github.com>
2026-07-09 06:33:38 +0200

    I forgot this

--- src/common/main/kotlin/hauveli/hexagony/config/HexagonyClientConfig.kt
+++ src/common/main/kotlin/hauveli/hexagony/config/HexagonyClientConfig.kt
@@ -8,9 +8,6 @@ import net.minecraft.network.chat.Component
 // guide: https://moddedmc.wiki/en/project/fzzy-config/latest/docs/config-design/New-Configs#2-config-creation
 class HexagonyClientConfig : Config(Hexagony.id("client_config")) {
 
-    var revealGreatSpellsOnHeldInBook = ValidatedBoolean(true).toCondition(
-        HexagonyConfigs.COMMON_CONFIG.requireScrollForAllGatedSpells,
-        Component.translatable("hexagony.config.patchouli_reveal.condition_not_met"),
-        { false })
+    var revealGreatSpellsOnHeldInBook = ValidatedBoolean(true)
 
 }
